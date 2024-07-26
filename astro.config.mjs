caefc5b464467ad02781ef10af6e742081b48f33
ShifterNathan <nathanielboydvillarroel@gmail.com>
2024-07-26 17:21:51 -0300

    Astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tourmaline-empanada-297c47.netlify.app/"
+  site: "https://tourmaline-empanada-297c47.netlify.app/",
+  integrations: [preact()]
 });
